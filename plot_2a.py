1cdc61c2096a7f7d8dfdbc5e094156ba97cdea7e
PeterR <piotrrutkowski97@gmail.com>
2020-04-04 20:41:32 +0200

    2b done

--- plot_2a.py
+++ plot_2a.py
@@ -2,19 +2,15 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
-# plot all AverageTemperatureCelsius vs. year
-
-# import data
 data = pd.read_csv("data/temperature_clean.csv")
 years = np.asarray(data['year'])
 average_temp_celsius = np.asarray(data['AverageTemperatureCelsius'])
 del data
 
-colors = ['dodgerblue', 'orchid', 'green', 'darkorange', 'brown']
-plt.scatter(years, average_temp_celsius, alpha= 1, edgecolor=((0, 0, 0, 1)), facecolors='none')
+plt.scatter(years, average_temp_celsius, edgecolor=((0, 0, 0, 1)), facecolors='none')
 
 plt.ylabel('AverageTemperatureCelsius')
-plt.xlabel('temperature_complete')
+plt.xlabel('year')
 plt.tight_layout()
 plt.ylim(-18, 32)
 plt.xlim(1730, 2025)
@@ -22,6 +18,3 @@ plt.xticks(np.arange(start=1750,stop=2050, step=50))
 plt.yticks(np.arange(start=-10, stop=32, step=10), rotation='vertical')
 plt.savefig('plots/2a.png', dpi=200)
 plt.clf()
-
-
-
