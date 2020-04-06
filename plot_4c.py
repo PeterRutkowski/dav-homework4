824ea2469485b811f1ba381a4fdfd8eeab1b6407
PeterR <piotrrutkowski97@gmail.com>
2020-04-06 23:36:41 +0200

    4c

--- plot_4c.py
+++ plot_4c.py
@@ -6,14 +6,13 @@ years, averages = import_data_4a()
 fig, ax = plot_axes_4()
 
 country_codes = ['Brazil', 'France', 'Japan', 'New Zealand', 'Poland', 'South Africa', 'Sweden', 'Ukraine']
+colors = [(1,0,0), (1,0,1), (0,1,0), (0,0,1), (1,1,0), (0,1,1), (0,0,0), (1,0.5,0)]
 
 for i in range(len(averages)):
-    ax.plot(years[i], averages[i], linewidth=1.2, label=country_codes[i])
+    ax.plot(years[i], averages[i], linewidth=1.2, label=country_codes[i], color=colors[i])
 
 plt.subplots_adjust(right=0.76)
-plt.legend(bbox_to_anchor=(1.,0.5), loc='center left', frameon=False)
-plt.text(2055, 15, 'Country', size='medium', horizontalalignment='center',
-         weight='bold', verticalalignment='center')
+plt.legend(bbox_to_anchor=(1.,0.5), loc='center left', frameon=False, title='Country')
 
 save_plot('4c')
 
