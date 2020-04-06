8442d646708713127d96cc1fe91bef678aa5fc5c
PeterR <piotrrutkowski97@gmail.com>
2020-04-06 22:46:14 +0200

    4c start

--- plot_4c.py
+++ plot_4c.py
@@ -5,7 +5,7 @@ years, averages = import_data_4a()
 fig, ax = plot_axes_4()
 
 for i in range(len(averages)):
-    ax.plot(years[i], averages[i], color='black', linewidth=1.2)
+    ax.plot(years[i], averages[i], linewidth=1.2)
 
-save_plot('4b')
+save_plot('4c')
 
